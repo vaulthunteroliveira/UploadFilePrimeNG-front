9007ecd97e5b8943225bed17cbca120da3417695
Alex Oliveira <alexoliverasince1989@hotmail.com>
2019-12-12 00:43:01 -0300

    escondendo botão de upload e usando o metodo onSelect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'UploadArquivosApp';
+
+
+  
+  uploadedFiles: any[] = [];
+
+  constructor() {}
+
+
+  dealWithFiles(event) {
+    
+      for(let file of event.files){
+        console.log(file.name)
+      }
+    
+    // for(let i = 0; i < fileList.lenght; i++){
+    //   this.uploadedFiles.push(fileList[i])
+    // }
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FileUploadModule} from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FileUploadModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
